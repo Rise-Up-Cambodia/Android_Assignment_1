81a000fe6bca5c1eba4efb8139d583e221adc153
Vanda <vandahor20@gmail.com>
2015-08-05 14:27:53 +0700

    Gift API

--- GiftApi/app/Controller/GiftsController.php
+++ GiftApi/app/Controller/GiftsController.php
@@ -3,7 +3,12 @@
     class GiftsController extends AppController{
        public  $components = array('RequestHandler');
 
+
+ //* Vanda 05/08/2015
+ // Get data from database //
+
         public function index() {
+
 //            $gifts = $this->Gift->find('all', array(
 //                'conditions' => array(
 //                    'status' => 1
@@ -12,11 +17,17 @@
 
             $gifts = $this->Gift->find('all',
                 array(
-                    'fields' => array('Gift.id', 'Gift.cat_id','Gift.description','Gift.from','Gift.date','Gift.status','recieve_date','name','category.name'),
+                    'fields' => array('Gift.id', 'Gift.cat_id','Gift.description','Gift.from','Gift.date','Gift.status','Gift.receive_date','Gift.gift_name','category.cat_name','user.name'),
                     'joins' => array(array('table' => 'categories',
                         'alias' => 'category',
                         'type' => 'INNER',
-                        'conditions' => array('category.id = Gift.cat_id','status'=>1),
+                        'conditions' => array('category.id = Gift.cat_id'),
+
+                    ),
+                   array('table' => 'users',
+                        'alias' => 'user',
+                        'type' => 'INNER',
+                        'conditions' => array('user.id = Gift.user_id','status'=>1),
 
                     )),
                      'order' => array(
@@ -28,14 +39,20 @@
                 '_serialize' => array('gifts')
             ));
         }
-        public function view($id) {
 
-            $gifts =  $this->Gift->findById($id);
+        public function view($id) {
+            $gifts =  $this->Gift->findById($id
+        );
+
             $this->set(array(
                 'gifts' => $gifts,
                 '_serialize' => array('gifts')
             ));
         }
+
+    //* Vanda 05/08/2015
+    // Add gift to database //
+
     public function add() {
 
         $this->Gift->create();
@@ -49,6 +66,11 @@
                     '_serialize' => array('message')
                 ));
         }
+
+
+        //* Vanda 05/08/2015
+        // Delete  gift to database by just update status to 0//
+
         public function edit($id) {
             $this->Gift->id = $id;
             if ($this->Gift->save($this->request->data)) {
@@ -61,17 +83,21 @@
                 '_serialize' => array('message')
             ));
         }
+
+        //* Vanda 05/08/2015
+        // Delete  gift to database by just update status to 0//
+
         public function delete($id) {
             $this->Gift->id = $id;
-            $image_name =  $this->Gift->find('all',array('fields' => "name","conditions"=>array("id"=>$id)));
+//            $image_name =  $this->Gift->find('all',array('fields' => "gift_name","conditions"=>array("id"=>$id)));
             if ($this->Gift->saveField('status', 0)) {
-                $file = new File(WWW_ROOT .'img/' .$image_name[0]['Gift']['name']);
-                if ($file->exists()) {
-                    $dir = new Folder(WWW_ROOT . 'img/delete_image', true);
-                    $file->copy($dir->path . DS . $file->name);
-                }
-               // $file = new File(WWW_ROOT . 'img/'.$image_name[0]['Gift']['image_path'], false, 0777);
-                $file->delete();
+//                $file = new File(WWW_ROOT .'img/' .$image_name[0]['Gift']['name']);
+//                if ($file->exists()) {
+//                    $dir = new Folder(WWW_ROOT . 'img/delete_image', true);
+//                    $file->copy($dir->path . DS . $file->name);
+//                }
+
+             //   $file->delete();
                 $message =   "Deleted";
             } else {
                 $message = 'Error';
