50e072ff292562bb087ab867a39453d8d2f81360
sreyleaktouch <sreyleaktouch1@gmail.com>
2015-08-05 14:22:52 +0700

    user controller completed

--- GiftApi/app/Config/routes.php
+++ GiftApi/app/Config/routes.php
@@ -1,10 +1,11 @@
 <?php
-    Router::mapResources('gifts');
+    Router::mapResources(array('gifts','users'));
     Router::resourceMap(array(
         array('action' => 'index', 'method' => 'GET', 'id' => false),
         array('action' => 'view', 'method' => 'GET', 'id' => true),
         array('action' => 'add', 'method' => 'POST', 'id' => false),
-        array('action' => 'edit', 'method' => 'PUT', 'id' => true),
+        array('action' => 'update', 'method' => 'PUT', 'id' => true),
+        array('action' => 'signup', 'method' => 'POST', 'id' => false),
         array('action' => 'delete', 'method' => 'DELETE', 'id' => true)
     ));
     Router::parseExtensions();
