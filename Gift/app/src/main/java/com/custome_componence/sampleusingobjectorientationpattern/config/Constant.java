2426be87ed98762d655cf502b9485664a418bab9
sreyleaktouch <sreyleaktouch1@gmail.com>
2015-08-07 09:05:32 +0700

    detail layout

--- Gift/app/src/main/java/com/custome_componence/sampleusingobjectorientationpattern/config/Constant.java
+++ Gift/app/src/main/java/com/custome_componence/sampleusingobjectorientationpattern/config/Constant.java
@@ -1,7 +1,7 @@
 package com.custome_componence.sampleusingobjectorientationpattern.config;
 
 /**
- * Created by Channy on 8/3/2015.
+ * Created by on 8/3/2015.
  */
 public class Constant {
     public static final String BASE_URL = "http://192.168.1.114:8585/Android_Assignment_1/GiftApi/";
